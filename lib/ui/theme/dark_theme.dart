45d976e66519f5835efb9d9ab1b576eaa2b751f6
Sihem Lerari <cs_lerari@outlook.com>
2024-06-25 15:52:56 +0100

    define app's theme

---
+++ lib/ui/theme/dark_theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff232539);
+const secondaryColor = Color(0xfffca611);
+const scaffoldBackgroundColor = Color(0xff211f2d);
+const defaultTextColor = Colors.white;
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: const ColorScheme.dark().copyWith(
+    primary: primaryColor,
+    secondary: secondaryColor,
+    onSecondary: Colors.white,
+    tertiary: const Color(0xff3b36bb),
+    surface: const Color(0xff3c4b70),
+    error: const Color(0xffd3323c),
+  ),
+  fontFamily: 'Poppins',
+  scaffoldBackgroundColor: scaffoldBackgroundColor,
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: Colors.white,
+    foregroundColor: primaryColor,
+  ),
+  appBarTheme: appBarTheme(),
+  textTheme: textTheme(),
+  elevatedButtonTheme: elevatedButtonTheme(),
+);
+
+AppBarTheme appBarTheme() => const AppBarTheme(
+      color: primaryColor,
+      elevation: 0,
+      centerTitle: true,
+      iconTheme: IconThemeData(color: Colors.white),
+    );
+
+TextTheme textTheme() => const TextTheme(
+      headlineMedium: TextStyle(
+        color: defaultTextColor,
+        fontWeight: FontWeight.bold,
+      ),
+      titleLarge: TextStyle(color: defaultTextColor),
+      titleMedium: TextStyle(color: defaultTextColor),
+      bodyLarge: TextStyle(color: defaultTextColor),
+      bodyMedium: TextStyle(color: defaultTextColor),
+      bodySmall: TextStyle(color: defaultTextColor),
+    );
+
+ElevatedButtonThemeData elevatedButtonTheme() => ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+      ),
+    );
---
+++ lib/ui/theme/sizes.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Sizes {
+  static const s4 = 4.0;
+  static const s8 = 8.0;
+  static const s12 = 12.0;
+  static const s16 = 16.0;
+  static const s20 = 20.0;
+  static const s24 = 24.0;
+  static const s32 = 32.0;
+  static const s48 = 48.0;
+  static const s64 = 64.0;
+}
+
+const gapW4 = SizedBox(width: Sizes.s4);
+const gapW8 = SizedBox(width: Sizes.s8);
+const gapW12 = SizedBox(width: Sizes.s12);
+const gapW16 = SizedBox(width: Sizes.s16);
+const gapW20 = SizedBox(width: Sizes.s20);
+const gapW24 = SizedBox(width: Sizes.s24);
+const gapW32 = SizedBox(width: Sizes.s32);
+const gapW48 = SizedBox(width: Sizes.s48);
+const gapW64 = SizedBox(width: Sizes.s64);
+
+const gapH4 = SizedBox(height: Sizes.s4);
+const gapH8 = SizedBox(height: Sizes.s8);
+const gapH12 = SizedBox(height: Sizes.s12);
+const gapH16 = SizedBox(height: Sizes.s16);
+const gapH20 = SizedBox(height: Sizes.s20);
+const gapH24 = SizedBox(height: Sizes.s24);
+const gapH32 = SizedBox(height: Sizes.s32);
+const gapH48 = SizedBox(height: Sizes.s48);
+const gapH64 = SizedBox(height: Sizes.s64);
