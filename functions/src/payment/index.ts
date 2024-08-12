6eb43eb5ddf55ef9900748f5f4b9c1ac06f44a2d
Sihem Lerari <cs_lerari@outlook.com>
2024-08-12 22:18:13 +0100

    export chargily payment feature

--- functions/src/payment/index.ts
+++ functions/src/payment/index.ts
@@ -1,2 +1,4 @@
 export { stripeCreatePaymentIntent } from "./stripe/stripeCreatePaymentIntent";
 export { onStripeSuccessfulPayment } from "./stripe/onStripeSuccessfulPayment";
+export { chargilyCreateCheckoutSession } from "./chargily/chargilyCreateCheckoutSession";
+export { onChargilySuccessfulPayment } from "./chargily/onChargilySuccessfulPayment";
