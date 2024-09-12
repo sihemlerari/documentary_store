4cdf333e116874e048475f3b57a572f193e4c410
Sihem Lerari <cs_lerari@outlook.com>
2024-09-12 22:29:13 +0100

    refactor order_list widget

--- lib/ui/orders/widgets/order_list.dart
+++ lib/ui/orders/widgets/order_list.dart
@@ -16,31 +16,56 @@ class OrderList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: orders.length,
-      itemBuilder: (context, index) {
-        final order = orders[index];
-        return ListTile(
-          title: Text('Order ID: ${order.id}'),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(order.documentaryName.toUpperCase()),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: Sizes.s4),
+      itemBuilder: (context, index) => OrderCard(order: orders[index]),
+    );
+  }
+}
+
+class OrderCard extends StatelessWidget {
+  final OrderSummary order;
+
+  const OrderCard({
+    super.key,
+    required this.order,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: Sizes.s8),
+      color: Theme.of(context).colorScheme.surface,
+      child: Padding(
+        padding: const EdgeInsets.all(Sizes.s12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Order ID: ${order.id}',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            gapH4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FormattedDate(dateTime: order.purchaseDate),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: Sizes.s8),
                   decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.tertiary,
                     borderRadius: BorderRadius.circular(Sizes.s8),
                   ),
-                  child: order.isExpired ? const Text('Expired') : const SizedBox.shrink(),
+                  child: order.isExpired ? const Text('expired') : const SizedBox.shrink(),
                 ),
-              ),
-            ],
-          ),
-          isThreeLine: true,
-          trailing: FormattedDate(dateTime: order.purchaseDate),
-        );
-      },
+              ],
+            ),
+            gapH16,
+            Text(
+              order.documentaryName.toUpperCase(),
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
